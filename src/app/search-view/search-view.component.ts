2321abc49cceacdfb1753100492ecf2df8a896a0
Nuttakit <nuttakit@heyday.ai>
2023-07-24 14:02:30 -0400

    feat: add search submit event handler with default tvshow list

--- src/app/search-view/search-view.component.ts
+++ src/app/search-view/search-view.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TvShowTableComponent} from '../tv-show-table/tv-show-table.component';
+import { TvShowService } from '../tv-show.service';
+import { TvShowSearch } from './type';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search-view',
   standalone: true,
   imports: [CommonModule, TvShowTableComponent],
   templateUrl: './search-view.component.html',
-  styleUrls: ['./search-view.component.css']
+  styleUrls: ['./search-view.component.css'],
+  providers: [TvShowService]
 })
 export class SearchViewComponent {
-
+  protected searchResult$: Observable<TvShowSearch> | undefined;
+  constructor(protected tvshow: TvShowService) {
+    this.searchResult$ = tvshow.searchTvShowByName();
+  }
 }
