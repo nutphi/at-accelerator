d931b5c264fe37cdd31be09acda0e6710fc2cc07
Nuttakit <nuttakit@heyday.ai>
2023-07-24 17:59:41 -0400

    feat: add search function

--- src/app/search-view/search-view.component.ts
+++ src/app/search-view/search-view.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {TvShowTableComponent} from '../tv-show-table/tv-show-table.component';
+import { TvShowService } from '../tv-show.service';
 
 @Component({
   selector: 'app-search-view',
   standalone: true,
   imports: [CommonModule, TvShowTableComponent],
   templateUrl: './search-view.component.html',
-  styleUrls: ['./search-view.component.css']
+  styleUrls: ['./search-view.component.css'],
+  providers: [TvShowService]
 })
 export class SearchViewComponent {
 
+  constructor(protected tvShow: TvShowService) {
+  }
+
+  onSearch(name: string, $event: Event) {
+    $event.preventDefault();
+    this.tvShow.changeSearchName(name);
+  }
 }
