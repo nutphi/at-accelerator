e8fc09fbe455814cc0cd5192563a90ae01c269c0
Nuttakit <nuttakit@heyday.ai>
2023-07-31 16:33:54 -0400

    feat: add routerlink into standalone component

--- src/app/tv-show-table/tv-show-table.component.ts
+++ src/app/tv-show-table/tv-show-table.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { DatePipe, NgIf, NgFor } from '@angular/common';
 import { TvShow, TvShowSearch } from '../type';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tv-show-table',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NgIf, NgFor, RouterLink, DatePipe],
   templateUrl: './tv-show-table.component.html',
   styleUrls: ['./tv-show-table.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush // if you have only input output, it is better to add ChangeDetectionStrategy.OnPush so the change detection won't trigger until the data changes
