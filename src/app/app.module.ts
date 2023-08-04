2cd173fdb3376afd95f2bfeb062d4a2c24fffb4b
Nuttakit <nuttakit@heyday.ai>
2023-08-04 11:23:23 -0400

    feat: change favorites view component into standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import FavoritesViewComponent from './favorites-view/favorites-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FavoritesViewComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- src/app/favorites-view/favorites-view.component.ts
+++ src/app/favorites-view/favorites-view.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-favorites-view',
+  standalone: true,
   templateUrl: './favorites-view.component.html',
   styleUrls: ['./favorites-view.component.css']
 })
