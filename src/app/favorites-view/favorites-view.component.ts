34d0b49a5f89e34f1a706a3e57a714556c29fe5f
Nuttakit <nuttakit@heyday.ai>
2023-08-09 19:53:53 -0400

    feat: applied to the favorites-view component to display favorites info

--- src/app/favorites-view/favorites-view.component.ts
+++ src/app/favorites-view/favorites-view.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FavoritesService } from '../favorites.service';
+import { NgIf, NgFor, DecimalPipe, I18nPluralPipe, JsonPipe, DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { MaxSeasonPipe } from '../max-season.pipe';
+import { StorageService } from '../storage.service';
+import { TvShowDetail } from '../type';
 
 @Component({
   selector: 'app-favorites-view',
   standalone: true,
+  imports: [NgIf, NgFor, RouterLink, DatePipe],
+  providers: [FavoritesService, StorageService],
   templateUrl: './favorites-view.component.html',
   styleUrls: ['./favorites-view.component.css']
 })
 export default class FavoritesViewComponent {
+  constructor(protected favorites: FavoritesService) { }
 
+  trackTvShowId(index: number, item: TvShowDetail) {
+    return item.id;
+  }
 }
