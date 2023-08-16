5c3e73bdec82c4f93fe6c3c258e2d287eec68e4a
Nuttakit <nuttakit@heyday.ai>
2023-08-16 09:42:15 -0400

    feat: apply fav-toggle directive & countdown pipe

--- src/app/favorite-card/favorite-card.component.ts
+++ src/app/favorite-card/favorite-card.component.ts
@@ -3,16 +3,19 @@ import { Component, Input } from '@angular/core';
 import { TvShowDetail } from '../type';
 import { FavoritesService } from '../favorites.service';
 import { RouterLink } from '@angular/router';
+import { CountDownPipe } from '../count-down.pipe';
+import { FavoriteToggleDirective } from '../favorite-toggle.directive';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-favorite-card',
   standalone: true,
-  imports: [NgIf, NgFor, RouterLink, DatePipe],
+  imports: [NgIf, NgFor, RouterLink, DatePipe, CountDownPipe, FavoriteToggleDirective],
   templateUrl: './favorite-card.component.html',
   styleUrls: ['./favorite-card.component.css']
 })
 export class FavoriteCardComponent {
   @Input() detail!: TvShowDetail;
 
-  constructor(protected favorites: FavoritesService) { }
+  constructor(protected favorites: FavoritesService, private dom: DomSanitizer) { }
 }
