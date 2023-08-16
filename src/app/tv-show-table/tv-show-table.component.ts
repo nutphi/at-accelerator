ba77b951a855c56844f67ec5357efec3c7350911
Nuttakit <nuttakit@heyday.ai>
2023-08-16 09:42:15 -0400

    feat: apply fav-toggle directive & countdown pipe

--- src/app/tv-show-table/tv-show-table.component.ts
+++ src/app/tv-show-table/tv-show-table.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
 import { DatePipe, NgIf, NgFor } from '@angular/common';
 import { TvShow, TvShowSearch } from '../type';
 import { RouterLink } from '@angular/router';
+import { FavoriteToggleDirective } from '../favorite-toggle.directive';
 
 @Component({
   selector: 'app-tv-show-table',
   standalone: true,
-  imports: [NgIf, NgFor, RouterLink, DatePipe],
+  imports: [NgIf, NgFor, RouterLink, DatePipe, FavoriteToggleDirective],
   templateUrl: './tv-show-table.component.html',
   styleUrls: ['./tv-show-table.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush // if you have only input output, it is better to add ChangeDetectionStrategy.OnPush so the change detection won't trigger until the data changes
