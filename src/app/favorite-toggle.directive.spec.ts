ac920ce7ea2898afe2438e040ce7cb5fd5334c4c
Nuttakit <nuttakit@heyday.ai>
2023-08-16 09:40:41 -0400

    feat: add favorite toggle directive

---
+++ src/app/favorite-toggle.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FavoriteToggleDirective } from './favorite-toggle.directive';
+
+describe('FavoriteToggleDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FavoriteToggleDirective();
+    expect(directive).toBeTruthy();
+  });
+});
