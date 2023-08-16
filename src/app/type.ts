8251089039c80b0574cebd785b493a674a99e4d4
Nuttakit <nuttakit@heyday.ai>
2023-08-16 09:40:18 -0400

    feat: add count-down pipe

--- src/app/type.ts
+++ src/app/type.ts
@@ -43,7 +43,7 @@ export interface TvShowDetail extends TvShow {
   image_path:           string;
   rating:               number;
   rating_count:         string;
-  countdown:            Episode;
+  countdown:            Episode | null;
   genres:               string[];
   pictures:             any[];
   episodes:             Episode[];
