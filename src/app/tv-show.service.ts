ddd5655f9e2f047df8d436a4d531248b0dd1f010
Nuttakit <nuttakit@heyday.ai>
2023-08-26 06:54:47 +0700

    feat: add switch map to trigger when the search api is slow (click prev then next)

--- src/app/tv-show.service.ts
+++ src/app/tv-show.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { DestroyRef, Injectable, Signal, effect, signal } from '@angular/core';
+import { Injectable, Signal, computed, signal } from '@angular/core';
 import { TvShowSearch } from './type';
-import { Observable } from 'rxjs';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Observable, switchMap } from 'rxjs';
+import { takeUntilDestroyed, toObservable } from '@angular/core/rxjs-interop';
 
 @Injectable()
 export class TvShowService {
@@ -23,15 +23,32 @@ export class TvShowService {
 
   private term_signal = signal<string>('');
 
-  constructor(private http: HttpClient, private destroyRef: DestroyRef) {
-    effect(() => {
+  // wrap computed with toObservable to trigger both term and page signals with observable to use switchmap (cancel the http request)
+  private search_trigger_signal: Observable<string> = toObservable(
+    computed(() => {
+      return `${this.term_signal()} ${this.page_signal()}`;
+    })
+  );
+
+  constructor(private http: HttpClient) {
+    this.search_trigger_signal.pipe(
+      switchMap(() => {
+      this.isLoading.set(true);
       const term = this.term_signal();
-      const result$ = !!term ? this.searchApi(term) : this.showPopularInfoApi();
-      result$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((tvshow: TvShowSearch) => { // angular 16 - takeUntilDestroyed
+      return term !== '' ? this.searchApi(term) : this.showPopularInfoApi();
+    }), takeUntilDestroyed())
+      .subscribe((tvshow: TvShowSearch) => {
         this.isLoading.set(false);
         this.searchResult.set(tvshow);
-      });
-    })
+    });
+    // effect(() => {
+    //   const term = this.term_signal();
+    //   const result$ = this.searchApi(term);
+    //   result$.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((tvshow: TvShowSearch) => { // angular 16 - takeUntilDestroyed with destroyRef will destroy the observable for sure once the service is destroyed
+    //     this.isLoading.set(false);
+    //     this.searchResult.set(tvshow);
+    //   });
+    // })
   }
 
   setPage(page: number) {
@@ -43,13 +60,14 @@ export class TvShowService {
     if (this.term_signal() === term) { // when term_signal is the same as before, not show isLoading
       return;
     }
-    this.isLoading.set(true);
-    this.searchResult.set(null);
     this.page_signal.set(1);
     this.term_signal.set(term);
   }
 
   private searchApi(q?: string): Observable<TvShowSearch>{
+    // it's ok to have multiple observable options since we use switchMap, but in effect on the comment better to use single observable to make sure the process is working well.
+    // const url = !!q ? `https://www.episodate.com/api/search` : `https://www.episodate.com/api/most-popular`;
+    // return this.http.get<TvShowSearch>(url, {params: {q: q ?? '', page: this.page_signal()}});
     return this.http.get<TvShowSearch>(`https://www.episodate.com/api/search`, {params: {q: q ?? '', page: this.page_signal()}});
   }
 
